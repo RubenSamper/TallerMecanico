40ffbe90fd5d0686d52396e6662de70bd231103a
RubenSamper <116352834+RubenSamper@users.noreply.github.com>
2024-05-21 11:50:02 +0200

    Creacion de la rama mariadb

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,14 +1,35 @@
 package org.iesalandalus.programacion.tallermecanico;
 
+import javafx.util.Pair;
 import org.iesalandalus.programacion.tallermecanico.controlador.Controlador;
 import org.iesalandalus.programacion.tallermecanico.controlador.IControlador;
 import org.iesalandalus.programacion.tallermecanico.modelo.FabricaModelo;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.FabricaFuenteDatos;
 import org.iesalandalus.programacion.tallermecanico.vista.FabricaVista;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Main {
-    public static void main(String[] args) {
-        IControlador controlador = new Controlador(FabricaModelo.CASCADA, FabricaFuenteDatos.FICHEROS, FabricaVista.GRAFICA);
+    public static void main(String[] args) throws OperationNotSupportedException {
+        Pair<FabricaVista, FabricaFuenteDatos> fabricas = procesarArgumentos(args);
+        IControlador controlador = new Controlador(FabricaModelo.CASCADA, fabricas.getValue(), FabricaVista.GRAFICA);
         controlador.comenzar();
+    }
+
+    private static Pair<FabricaVista, FabricaFuenteDatos> procesarArgumentos(String[] args) {
+        FabricaVista fabricaVista = FabricaVista.GRAFICA;
+        FabricaFuenteDatos fabricaFuenteDatos = FabricaFuenteDatos.MARIADB;
+        for (String argumento : args) {
+            if (argumento.equalsIgnoreCase("vventanas")) {
+                fabricaVista = FabricaVista.GRAFICA;
+            } else if (argumento.equalsIgnoreCase("vtexto")) {
+                fabricaVista = FabricaVista.TEXTO;
+            } else if (argumento.equalsIgnoreCase("fdficheros")) {
+                fabricaFuenteDatos = FabricaFuenteDatos.FICHEROS;
+            } else if (argumento.equalsIgnoreCase("fdmariadb")) {
+                fabricaFuenteDatos = FabricaFuenteDatos.MARIADB;
+            }
+        }
+        return new Pair<>(fabricaVista,fabricaFuenteDatos);
     }
 }
--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
@@ -1,6 +1,8 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.FuenteDatosFicheros;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.mariadb.FuenteDatosMariaDB;
+import org.iesalandalus.programacion.tallermecanico.vista.Vista;
 
 public enum FabricaFuenteDatos {
 
@@ -8,6 +10,13 @@ public enum FabricaFuenteDatos {
         @Override
         public IFuenteDatos crear() {
             return new FuenteDatosFicheros();
+        }
+    },
+
+    MARIADB {
+        @Override
+        public IFuenteDatos crear() {
+            return new FuenteDatosMariaDB();
         }
     };
 
